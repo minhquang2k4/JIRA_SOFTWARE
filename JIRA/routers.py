d0263222a46251acd7d647cdedb57dfa845a1d68
minhquang2k4 <minhwang1708@gmail.com>
2024-10-15 15:33:56 +0700

    register

--- JIRA/routers.py
+++ JIRA/routers.py
@@ -1,6 +1,16 @@
 from flask import render_template, flash, session, redirect, url_for
+from functools import wraps
 
 from JIRA import app
+
+
+# def login_required(f):
+#   @wraps(f)
+#   def decorated_function(*args, **kwargs):
+#     if not current_user.is_authenticated:
+#       return redirect(url_for('login'))
+#     return f(*args, **kwargs)
+#   return decorated_function
 
 @app.route('/')
 def home():
